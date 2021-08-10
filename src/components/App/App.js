93120c56843c6f892d20bdf1c9fb2286dc6ba34c
Zamira <radzh.zamira@gmail.com>
2021-08-10 21:40:19 +0300

    дз к 19 уроку, Props

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
+
+
+const todoItem = 'Выполнить задание';
+const App = () => (
+  <div>
+    <h1>Важные дела:</h1>
+    <ItemList todoItem={todoItem}/>
+    <Footer count={4} />
+  </div>);
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = ({ count }) => (<div>Осталось выполнить вот столько дел: {count}</div>);
+
+export default Footer;
